d1c6d9493976664e86b9b83f2531419bb0abf99f
Subodh Kumar <snp42u@gmail.com>
2022-08-24 09:45:55 +0530

    edited

---
+++ loops.js
@@ -0,0 +1,15 @@
+var name = "The Romeo";
+for (let i = 0; i < name.length; i++) {
+  console.log(name[i]);
+}
+var i = 1
+while (i<10) {
+    console.log(i)
+    i++
+}
+
+var x=10
+do{
+    console.log(x)
+    x--
+}while(x>0)
