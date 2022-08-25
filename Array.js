609fdb532f6785c70bc857f61e65dab74b6fe3e2
Subodh Kumar <snp42u@gmail.com>
2022-08-25 23:43:33 +0530

    new code

---
+++ Array.js
@@ -0,0 +1,7 @@
+var tree = ['redwood','bay','ccedar','oak','maple']
+console.log(0 in tree)
+console.log(3 in tree)
+console.log(6 in tree)
+console.log('bay' in tree)
+console.log('length' in tree)
+console.log('Romeo' in tree)
---
+++ functiontype.js
@@ -0,0 +1,15 @@
+var myFun = new Function('5+2')
+var Fun = new Function('msg')
+var shape = 'round'
+var size = 1
+var foo = ['Aapple','Mango','Orange']
+var today = new Date()
+console.log(typeof myFun)
+console.log(typeof Fun)
+console.log(typeof shape)
+console.log(typeof size)
+console.log(typeof foo)
+console.log(typeof today)
+console.log(typeof Date)
+console.log(typeof date)
+console.log(typeof novalue)
